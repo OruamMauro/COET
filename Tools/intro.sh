019a2199153640a7f8f17fbd9acbe2254b3dad33
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2023-07-24 23:42:17 -0600

    modificando intro.sh

--- Tools/intro.sh
+++ Tools/intro.sh
@@ -6,15 +6,18 @@ RED="\e[31m"
 L_MAGENTA="\e[95m"
 END="\e[0m"
 
+set -o allexport
+
 # Function to validate if the code is alphanumeric and has 10 characters
 function validate_code() {
   read -p "Enter an alphanumeric text of 10 characters: " code
   if [[ "$code" =~ ^[[:alnum:]]{10}$ ]]; then
     echo -e "The code ${CYAN}\"$code\"${END} is valid!"
     echo $code > /tmp/.USER_CODE
+    source /opt/.Fe7ege5/Tools/set_vars.sh
     exit 0
   else
-    echo "Error: The code ${RED}\"$code\"${END} is not valid. It should be alphanumeric and have 10 characters."
+    echo -e "Error: The code ${RED}\"$code\"${END} is not valid. It should be alphanumeric and have 10 characters."
     validate_code
   fi
 }
