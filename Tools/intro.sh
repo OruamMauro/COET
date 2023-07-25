8b223825c85ab7fbe76b49453d3272eaf4920944
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2023-07-24 23:19:03 -0600

    creando script de herramientas

---
+++ Tools/intro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CYAN="\e[36m"
+YELLOW="\e[33m"
+RED="\e[31m"
+L_MAGENTA="\e[95m"
+END="\e[0m"
+
+# Function to validate if the code is alphanumeric and has 10 characters
+function validate_code() {
+  read -p "Enter an alphanumeric text of 10 characters: " code
+  if [[ "$code" =~ ^[[:alnum:]]{10}$ ]]; then
+    echo -e "The code ${CYAN}\"$code\"${END} is valid!"
+    echo $code > /tmp/.USER_CODE
+    exit 0
+  else
+    echo "Error: The code ${RED}\"$code\"${END} is not valid. It should be alphanumeric and have 10 characters."
+    validate_code
+  fi
+}
+
+validate_code
---
+++ Tools/set_vars.sh
@@ -0,0 +1,3 @@
+export REP0_D1R=/opt/.Fe7ege5
+export REC_DIR=/tmp/$(cat /tmp/.USER_CODE)
+alias StartScenario='bash ${REP0_DIR}/Tools/intro.sh'
