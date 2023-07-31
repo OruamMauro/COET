2d95d1a27e69fea05dd65ae9aaca9858b05db101
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2023-07-31 17:26:14 -0600

    modificando test.sftp.sh

--- Tools/test-sftp.sh
+++ Tools/test-sftp.sh
@@ -2,10 +2,10 @@
 
 # Function to display script usage
 function show_usage() {
-    echo -e "\nUsage: $0 -i|--identity_file <ssh_key_file> -f|--file <file_name>\n\n"
+    echo -e "\nUsage: $0 -f|--file <file_name>\n\n"
     echo -e "This program helps you to validate the connectivity to the SFTP server wano.kuni\n"
     echo -e "Create an empty file and use it to test the connectivity.\n"
-    echo -e "Note: The identity file you will use should be with no passphrase.\n"
+    echo -e "Note: The identity file (/home/luffy/.ssh/id_rsa) should be with no passphrase.\n"
     exit 1
 }
 
@@ -17,10 +17,6 @@ while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
-        -i|--identity_file)
-            shift
-            ssh_key="$1"
-            ;;
         -f|--file)
             shift
             file_name="$1"
@@ -46,22 +42,22 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Check if required options are provided
-if [ -z "$ssh_key" ] || [ -z "$file_name" ]; then
+if [ -z "$file_name" ]; then
     echo "Error: Missing required options."
     show_usage
 fi
 
 # Check if the file exists
-if [ ! -f "$ssh_key" ]; then
-    echo "Error: SSH key file not found."
+if [ ! -f "$file_name" ]; then
+    echo "Error: File $file_name not found."
     exit 1
 fi
 
 # Upload or download the file based on the specified action
 if [ "$action" == "put" ]; then
     echo "Uploading $file_name to the server..."
-    sftp -oNumberOfPasswordPrompts=0 -i "$ssh_key" -P 8022 luffytarou@wano.kuni:/onigashima/ <<< "put $file_name"
+    sftp -oNumberOfPasswordPrompts=0 -i /home/luffy/.ssh/id_rsa -P 8022 luffytarou@wano.kuni:/onigashima/ <<< "put $file_name"
 elif [ "$action" == "get" ]; then
     echo "Downloading $file_name from the server..."
-    sftp -oNumberOfPasswordPrompts=0 -i "$ssh_key" -P 8022 luffytarou@wano.kuni:/onigashima/ <<< "get $file_name /opt/adenza/07/dw/$file_name"
+    sftp -oNumberOfPasswordPrompts=0 -i /home/luffy/.ssh/id_rsa -P 8022 luffytarou@wano.kuni:/onigashima/ <<< "get $file_name /opt/adenza/07/dw/$file_name"
 fi
