446a6c4c24091bde0b3a9418d3cfe69a54ddc2d0
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2023-08-04 16:38:53 -0600

    modificando syncdata.sh y crontab

--- Tools/syncdata.sh
+++ Tools/syncdata.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-DATE=$(date +%F_%H-%M)
-NAME=$(cat /tmp/.USER_NAME | tr "[:blank:]" "_")
+DATE=$(date +%F)
 
-mkdir -p /home/ubuntu/.config/.Fe7ege5/Recordings/tmp/$(echo -n $NAME)_${DATE}
-cp /tmp/asciinema/rec.cast /home/ubuntu/.config/.Fe7ege5/Recordings/tmp/$(echo -n $NAME)_${DATE}
+mkdir -p /home/ubuntu/.config/.Fe7ege5/Recordings/tmp/${DATE}
+cp /tmp/asciinema/rec.cast /home/ubuntu/.config/.Fe7ege5/Recordings/tmp/${DATE}
+echo "$(date)" >> /home/ubuntu/.config/.Fe7ege5/Recordings/tmp/${DATE}/terminals.txt
+ps -fea|grep pts >> /home/ubuntu/.config/.Fe7ege5/Recordings/tmp/${DATE}/terminals.txt
 cd /home/ubuntu/.config/.Fe7ege5/
 git pull --rebase
 git add *
