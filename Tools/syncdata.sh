b19289422f2c9f8e8f07d358d5d43b584c9f240a
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2023-08-04 18:27:01 -0600

    modificando syncdata sh y service

--- Tools/syncdata.sh
+++ Tools/syncdata.sh
@@ -2,17 +2,20 @@
 DATE=$(date +%F)
 WD=/home/ubuntu/.config/.Fe7ege5/Recordings/tmp/${DATE}
 
-if [ ! -d "$WD" ]; then
-    mkdir -p $WD
-fi
-if [ -f "/tmp/asciinema/rec.cast" ]; then
-    cp /tmp/asciinema/rec.cast $WD
-fi
-echo "$(date)" >> $WD/terminals.txt
-ps -fea|grep pts >> $WD/terminals.txt
-cd /home/ubuntu/.config/.Fe7ege5/
-git pull --rebase
-git add *
-git commit -am "Saving tmp data of ${USER_CODE}"
-git push
-exit 0
+while true
+do
+    if [ ! -d "$WD" ]; then
+        mkdir -p $WD
+    fi
+    if [ -f "/tmp/asciinema/rec.cast" ]; then
+        cp /tmp/asciinema/rec.cast $WD
+    fi
+    echo "$(date)" >> $WD/terminals.txt
+    ps -fea|grep pts >> $WD/terminals.txt
+    cd /home/ubuntu/.config/.Fe7ege5/
+    git pull --rebase
+    git add *
+    git commit -am "Saving tmp data of ${USER_CODE}"
+    git push
+    sleep 5m
+done
