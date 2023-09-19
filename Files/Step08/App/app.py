5cbbfef2912bd3f7024facb1cf59b3bc10533041
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2023-09-19 17:42:53 -0600

    actualizando md5 check sum de hint

--- Files/Step08/App/app.py
+++ Files/Step08/App/app.py
@@ -10,7 +10,7 @@ def process_key():
     key = data.get('key')
 
     #if key and isinstance(key, str) and len(key) == 32:
-    if key == "2113819ee4ab8fe37db33d02d7893868":
+    if key == "24b7ca3c8facfb39793ad82e595c6a13":
         return jsonify({'encoded_message_64': 'Q3JlYXRlIGEgZmlsZSBuYW1lZCAnZXhfMDgudHh0JyBpbiB0aGUgJy90bXAnIGRpcmVjdG9yeSBhbmQgYWRkIHRoZSB0ZXh0ICdTaGFua3MnLgo='}), 200
     else:
         return jsonify({'error': 'Invalid key.'}), 400
